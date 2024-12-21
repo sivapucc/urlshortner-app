01b3abf56395809768adc480472813b1188b708a
sivapucc <sivapucc@gmail.com>
2024-12-21 23:56:56 +0530

    modification done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { reportWebVitals } from "web-vitals";
+import reportWebVitals from "./reportWebVitals";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
